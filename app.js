e026c0515ebb9930fe2ee5bdf534cfdc11934d02
Sukhpal Saini <bdcorps@gmail.com>
2019-11-01 07:17:55 -0400

    add email capture

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require("express"),
   path = require("path"),
-  app = express();
+  app = express(),
+  bodyParser = require("body-parser");
 
 var port = process.env.PORT || 3000;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+var emails = [];
 
 app.use(express.static(path.resolve(__dirname, "public")));
 
@@ -12,6 +22,16 @@ app.get("/", function(req, res) {
 
 app.get("/app", function(req, res) {
   res.sendFile(path.join(__dirname, "public", "app.html"));
+});
+
+app.post("/submit", function(req, res) {
+  emails.push(req.body.email);
+  res.send("Thank you for subscribing! Check us out on Product Hunt.");
+});
+
+app.get("/list", function(req, res) {
+  const emailsList = JSON.stringify(emails);
+  res.send(emailsList);
 });
 
 var server = app.listen(port, function() {
